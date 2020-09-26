ef0913bb7bd1a7f6b1d258c731afe897d8ba5123
Taylor Booth <taylorkbooth@gmail.com>
2020-09-26 01:54:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,9 +55,13 @@ class Owner
   end
   
   def sell_pets
-    @pets.each do |o| 
-      expect(o.owner) == (nil) }
+    @pets.each do |type, name_array|
+      name_array.each do |pet|
+        pet.mood = "nervous"
+        #name_array.delete(pet)
+      end 
     end
+    @pets = {}
   end
   
    def list_pets
